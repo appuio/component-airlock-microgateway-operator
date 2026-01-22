9975f05530f1617dad977e19603ca6f090d6b938
Simon Gerber <simon.gerber@vshn.ch>
2026-01-22 16:27:33 +0100

    Update approval script to use `kubectl get installplan`
    On OpenShift 4.20 `ip` expands to `ipaddresses.networking.k8s.io`
    instead of installplans.

--- component/scripts/approve.sh
+++ component/scripts/approve.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-AIRLOCK_INSTALLPLAN=$(kubectl -n "${AIRLOCK_NAMESPACE}" get ip -ojson | jq -r --argjson upgrade_job "${JOB_metadata_creationTimestamp}" '.items | sort_by(.metadata.creationTimestamp) | reverse | [.[] |  select((.spec.approved != true) and (.metadata.creationTimestamp < $upgrade_job))][0] | .metadata.name')
+AIRLOCK_INSTALLPLAN=$(kubectl -n "${AIRLOCK_NAMESPACE}" get installplan -ojson | jq -r --argjson upgrade_job "${JOB_metadata_creationTimestamp}" '.items | sort_by(.metadata.creationTimestamp) | reverse | [.[] |  select((.spec.approved != true) and (.metadata.creationTimestamp < $upgrade_job))][0] | .metadata.name')
 if [ "${AIRLOCK_INSTALLPLAN}" != "null" ]; then
   kubectl patch installplan "${AIRLOCK_INSTALLPLAN}" -n "${AIRLOCK_NAMESPACE}" --type merge --patch '{"spec":{"approved":true}}';
 fi
